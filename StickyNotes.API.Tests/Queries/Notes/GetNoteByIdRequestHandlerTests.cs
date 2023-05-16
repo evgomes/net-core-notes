5b8bfb9763e2cc4836146eb4bf6ebcadb508e2fd
Evandro Gomes <evandro.ggomes@outlook.com>
2023-05-16 12:50:17 -0300

    Added unit tests for queries.

--- StickyNotes.API.Tests/Queries/Notes/GetNoteByIdRequestHandlerTests.cs
+++ StickyNotes.API.Tests/Queries/Notes/GetNoteByIdRequestHandlerTests.cs
@@ -1,12 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.EntityFrameworkCore;
+using StickyNotesCore.API.Domain.Commands.Notes;
+using StickyNotesCore.API.Domain.Data.Contexts;
+using StickyNotesCore.API.Domain.Models;
+using StickyNotesCore.API.Domain.Queries.Notes;
+using StickyNotesCore.API.Queries.Notes;
+using StickyNotesCore.Shared.Responses;
 
 namespace StickyNotes.API.Tests.Queries.Notes
 {
-	internal class GetNoteByIdRequestHandlerTests
+	public class GetNoteByIdRequestHandlerTests
 	{
+		private Mock<StickyNotesContext> _context; 
+		private GetNoteByIdRequestHandler _commandHandler;
+
+		public GetNoteByIdRequestHandlerTests()
+		{
+			_context = new();
+			_commandHandler = new GetNoteByIdRequestHandler(_context.Object);
+		}
+
+		[Trait("Category", "Get Note by ID")]
+		[Fact]
+		public async Task Should_Return_Not_Found_For_Invalid_Note_Id()
+		{
+			// Arrange
+			var notes = new List<Note>();
+
+			_context
+				.SetupGet(x => x.Notes)
+				.ReturnsDbSet(notes);
+
+			var query = BuildQuery();
+
+			// Act
+			var response = await _commandHandler.Handle(query, CancellationToken.None);
+
+			// Assert
+			Assert.False(response.Success);
+			Assert.Matches("not found", response.Message);
+		}
+
+		[Trait("Category", "Get Note by ID")]
+		[Fact]
+		public async Task Should_Return_Note_by_ID()
+		{
+
+			var notes = new List<Note>
+			{
+				new Note
+				{
+					Id = Guid.Parse("b87762bb-3340-4baa-9f94-529ba9a3a44c"),
+					Text = "Sample Note",
+					CreatedOn = DateTime.UtcNow,
+				}
+			};
+
+			_context
+				.SetupGet(x => x.Notes)
+				.ReturnsDbSet(notes);
+
+			var query = BuildQuery();
+
+			// ACt
+			var response = await _commandHandler.Handle(query, CancellationToken.None);
+
+			Assert.True(response.Success);
+			Assert.NotNull(response.Resource);
+			Assert.Equal(Guid.Parse("b87762bb-3340-4baa-9f94-529ba9a3a44c"), response.Resource.Id);
+			Assert.Equal("Sample Note", response.Resource.Text);
+			Assert.Equal(Status.Completed, response.Status);
+		}
+
+		private GetNoteByIdRequest BuildQuery()
+		{
+			return new GetNoteByIdRequest
+			{
+				Id = Guid.Parse("b87762bb-3340-4baa-9f94-529ba9a3a44c"),
+			};
+		}
 	}
 }
--- StickyNotes.API.Tests/Queries/Notes/ListNotesRequestHandlerTests.cs
+++ StickyNotes.API.Tests/Queries/Notes/ListNotesRequestHandlerTests.cs
@@ -1,12 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using Moq.EntityFrameworkCore;
+using StickyNotesCore.API.Domain.Data.Contexts;
+using StickyNotesCore.API.Domain.Data.Queries.Notes;
+using StickyNotesCore.API.Domain.Data.Queries.Shared;
+using StickyNotesCore.API.Domain.Models;
+using StickyNotesCore.API.Domain.Queries.Notes;
+using StickyNotesCore.API.Queries.Notes;
 
 namespace StickyNotes.API.Tests.Queries.Notes
 {
-	internal class ListNotesRequestHandlerTests
+	public class ListNotesRequestHandlerTests
 	{
+		private Mock<StickyNotesContext> _context;
+		private ListNotesRequestHandler _commandHandler;
+
+		public ListNotesRequestHandlerTests()
+		{
+			_context = new();
+			_commandHandler = new ListNotesRequestHandler(_context.Object);
+		}
+
+		[Trait("Category", "List Notes")]
+		[Fact]
+		public async Task Should_Return_Empty_Query_Result_When_There_Are_No_Notes_In_The_Database()
+		{
+			// Arrange
+			var notes = new List<Note>();
+
+			_context
+				.SetupGet(x => x.Notes)
+				.ReturnsDbSet(notes);
+
+			// Act
+			var response = await _commandHandler.Handle(new ListNotesRequest
+			{
+				Query = new NotesQuery
+				{
+					Page = 1,
+					ItemsPerPage = 10,
+					SortOrder = SortOrder.Ascending,
+				}
+			}, CancellationToken.None);
+
+			Assert.NotNull(response);
+			Assert.Equal(0, response.TotalItems);
+			Assert.Empty(response.Items);
+		}
+
+		[Trait("Category", "List Notes")]
+		[Fact]
+		public async Task Should_Return_Notes_According_To_Query()
+		{
+			// Arrange
+			var notes = new List<Note>
+			{
+				new Note
+				{
+					Id = Guid.Parse("b87762bb-3340-4baa-9f94-529ba9a3a44c"),
+					Text = "A Note",
+					CreatedOn = DateTime.UtcNow,
+				},
+				new Note
+				{
+					Id = Guid.Parse("e0abfb9c-a178-4cf4-b3fa-fa50dc790e5a"),
+					Text = "B Note",
+					CreatedOn = DateTime.UtcNow,
+				},
+				new Note
+				{
+					Id = Guid.Parse("2d7b3fed-f68e-40e3-9ce3-fdefcd29c9e1"),
+					Text = "C Note",
+					CreatedOn = DateTime.UtcNow,
+				},
+			};
+
+			_context
+				.SetupGet(x => x.Notes)
+				.ReturnsDbSet(notes);
+
+			// Act
+			var response = await _commandHandler.Handle(new ListNotesRequest
+			{
+				Query = new NotesQuery
+				{
+					Text = "Note",
+					Page = 1,
+					ItemsPerPage = 10,
+					OrderBy = nameof(Note.Text),
+					SortOrder = SortOrder.Descending,
+				}
+			}, CancellationToken.None);
+
+			// Assert
+			Assert.NotNull(response);
+			Assert.Equal(3, response.TotalItems);
+			Assert.Equal("C Note", response.Items[0].Text);
+			Assert.Equal("B Note", response.Items[1].Text);
+			Assert.Equal("A Note", response.Items[2].Text);
+		}
 	}
 }
